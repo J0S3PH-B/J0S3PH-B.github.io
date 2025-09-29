fa5bbe664d8e740ed0776e4e497fc02c90e0e6ad
Joseph-B <rbt.joseph2811@gmail.com>
2025-09-29 22:56:54 +0700

    Deploy built site to GitHub Pages

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -1,12 +1,12 @@
-const projects = [
-    {
+// const projects = [
+//     {
 
-    },
-    {
+//     },
+//     {
 
-    },
+//     },
     
-]
+// ]
 
 export const Projects = () => {
     return (
